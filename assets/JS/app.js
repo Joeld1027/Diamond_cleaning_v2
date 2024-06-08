4a81e515c27566fffab8050cc73f83688b43c121
Joel Infante <joelinfante@Joels-iMac.local>
2024-06-08 18:39:13 -0500

    added service section

---
+++ assets/JS/app.js
@@ -0,0 +1,7 @@
+const serviceTab = document.querySelector("#home-service");
+
+serviceTab.addEventListener("click", function (e) {
+  console.log("div was clicked");
+});
+
+console.log(serviceTab);
