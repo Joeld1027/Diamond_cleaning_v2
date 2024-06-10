6f8e8acd28935eb5234ca35b8142785a5316942a
Joel Infante <joelinfante@Joels-iMac.local>
2024-06-09 23:13:37 -0500

    added service section with js functionality,addd gallery and testimonials

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,2 +1,17 @@
+const activeTab = document.querySelector(".active-og");
+const allLinks = document.querySelectorAll(".tablink");
+const allImgs = document.querySelectorAll(".tabcontent");
 
+const activeImg = document.querySelector("." + activeTab.id);
+activeImg.classList.remove("d-none");
 
+allLinks.forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    allLinks.forEach((elem) => elem.classList.remove("active-og"));
+    e.currentTarget.classList.add("active-og");
+    allImgs.forEach((img) => {
+      img.classList.add("d-none");
+    });
+    document.querySelector("." + e.currentTarget.id).classList.remove("d-none");
+  });
+});
